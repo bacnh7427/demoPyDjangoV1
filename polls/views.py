b80a689dc6b502ce87b7830e564370e484d43c55
bacnh7427 <bacnh7427@co-well.com.vn>
2023-04-05 17:37:06 +0700

    add source base

---
+++ polls/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+
+    return HttpResponse("Hello Django")
+
+def getUser(request):
+    return HttpResponse("<h1>Get all User vote</h1>")
