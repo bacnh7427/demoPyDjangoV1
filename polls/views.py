5f7b07c327eee4933a18e5a7021426eb94208683
bacnh7427 <bacnh7427@co-well.com.vn>
2023-04-06 09:51:05 +0700

    add templates, send data to views

--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-
-    return HttpResponse("Hello Django")
+    myname = "Bac cowell dev"
+    mySkill = ["python", "django", 'ruby', "java", "golang"]
+    context = {"name": myname, 'skill': mySkill}
+    return render(request, "polls/index.html", context)
 
 def getUser(request):
     return HttpResponse("<h1>Get all User vote</h1>")
