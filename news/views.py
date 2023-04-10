fa3e63e9ce9925fe82a1de70603689f9071a546c
bacnh7427 <bacnh7427@co-well.com.vn>
2023-04-10 17:30:23 +0700

    features sending email

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import PostForm
+from .forms import PostForm, SendEmail
+
+
 # Create your views here.
 
 
@@ -21,5 +23,27 @@ def save_news(request):
             return HttpResponse('Data saved. OK!')
         else:
             return HttpResponse('Data not validate')
+    else:
+        return HttpResponse("Not request post")
+
+
+def email_view(request):
+    b = SendEmail()
+    return render(request, 'news/email.html', {'f': b})
+
+
+def send_email_complete(request):
+    if request.method == "POST":
+        m = SendEmail(request.POST)
+        if m.is_valid():
+            #context2 = {'email': m}
+            title = m.cleaned_data['title']
+            content = m.cleaned_data['content']
+            email = m.cleaned_data['email']
+            cc = m.cleaned_data['cc']
+            context = {'tt': title, 'ct': content, 'em': email, 'cc': cc}
+            return render(request, 'news/email_complete.html', {'email': context})
+        else:
+            return HttpResponse("Email not valid, sending error. Please check your email")
     else:
         return HttpResponse("Not request post")
