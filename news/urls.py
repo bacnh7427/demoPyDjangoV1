4af104d5ae0c1c0bd9f3cd276dc764d99ac94b60
bacnh7427 <bacnh7427@co-well.com.vn>
2023-04-11 09:32:28 +0700

    change function base view to class base view

--- news/urls.py
+++ news/urls.py
@@ -2,9 +2,9 @@ from . import views
 from django.urls import path
 app_name = 'news'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('add/', views.add_post, name='add_data'),
-    path('save/', views.save_news, name='save_data'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('add/', views.SendData.as_view(), name='add_data'),
+    path('save/', views.SaveData.as_view(), name='save_data'),
     path('send_email', views.email_view, name='send_email'),
     path('send_email_complete', views.send_email_complete, name='send_email_complete'),
 
--- news/views.py
+++ news/views.py
@@ -1,30 +1,33 @@
+# Class base view
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import PostForm, SendEmail
+from django.views import View
 
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Form news")
+class IndexView(View):
+    def get(self, request):
+        dt = "Notification in scr"
+        return HttpResponse(dt)
 
 
-def add_post(request):
-    post_data = PostForm()
-    return render(request, 'news/add_news.html', {'f': post_data})
+class SendData(View):
+    def get(self, request):
+        post_data = PostForm()
+        return render(request, 'news/add_news.html', {'f': post_data})
 
 
-def save_news(request):
-    if request.method == "POST":
+class SaveData(View):
+    def post(self, request):
         g = PostForm(request.POST)
         if g.is_valid():
             g.save()
             return HttpResponse('Data saved. OK!')
         else:
             return HttpResponse('Data not validate')
-    else:
-        return HttpResponse("Not request post")
 
 
 def email_view(request):
@@ -47,3 +50,55 @@ def send_email_complete(request):
             return HttpResponse("Email not valid, sending error. Please check your email")
     else:
         return HttpResponse("Not request post")
+
+
+# Function base view
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from .forms import PostForm, SendEmail
+#
+#
+# # Create your views here.
+#
+#
+# def index(request):
+#     return HttpResponse("Form news")
+#
+#
+# def add_post(request):
+#     post_data = PostForm()
+#     return render(request, 'news/add_news.html', {'f': post_data})
+#
+#
+# def save_news(request):
+#     if request.method == "POST":
+#         g = PostForm(request.POST)
+#         if g.is_valid():
+#             g.save()
+#             return HttpResponse('Data saved. OK!')
+#         else:
+#             return HttpResponse('Data not validate')
+#     else:
+#         return HttpResponse("Not request post")
+#
+#
+# def email_view(request):
+#     b = SendEmail()
+#     return render(request, 'news/email.html', {'f': b})
+#
+#
+# def send_email_complete(request):
+#     if request.method == "POST":
+#         m = SendEmail(request.POST)
+#         if m.is_valid():
+#             #context2 = {'email': m}
+#             title = m.cleaned_data['title']
+#             content = m.cleaned_data['content']
+#             email = m.cleaned_data['email']
+#             cc = m.cleaned_data['cc']
+#             context = {'tt': title, 'ct': content, 'em': email, 'cc': cc}
+#             return render(request, 'news/email_complete.html', {'email': context})
+#         else:
+#             return HttpResponse("Email not valid, sending error. Please check your email")
+#     else:
+#         return HttpResponse("Not request post")
