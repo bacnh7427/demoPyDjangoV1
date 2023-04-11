b7ae0e4200f3f0448ecc54fc384c4cdd323f2791
bacnh7427 <bacnh7427@co-well.com.vn>
2023-04-11 14:48:22 +0700

    add screen login, authenticate for user/pw

--- login/views.py
+++ login/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from django.contrib.auth import authenticate
 
 
 class Login(View):
+
     def get(self, request):
         dt = "Login screen"
         return render(request, "login/login.html")
+
+    def post(self, request):
+        tendangnhap = request.POST.get('username')
+        matkhau = request.POST.get('password')
+        user_login = authenticate(username = tendangnhap, password = matkhau)
+        if user_login is None:
+            return HttpResponse('User khong ton tai')
+        return HttpResponse("Ban vua bam dang nhap %s %s" %(tendangnhap, matkhau))
